346b7a7928cbd2ebb77b688570e5c33f2f0599c8
cuongcamauit <cuongcamauit@gmail.com>
2021-09-26 10:12:46 +0700

    verson 1

---
+++ process.py
@@ -0,0 +1,9 @@
+import sqlite3
+con = sqlite3.connect('datasv.db')
+cur = con.cursor()
+cur.execute("""INSERT INTO DSSV VALUES ('S02', 'DF', '55') """)
+con.commit()
+result = cur.execute("""select *
+                        from DSSV """)
+for i in result:
+    print(i)
